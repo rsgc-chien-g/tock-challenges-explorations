bf487977dae7227af8bf65db5af864b794428016
rsgc-chien-g <student@MacBook-Pro-0681-GChien.local>
2016-02-03 10:38:50 -0500

    Finished the challenge

--- Tock-Challenges/Day3-1.playground/Contents.swift
+++ Tock-Challenges/Day3-1.playground/Contents.swift
@@ -2,4 +2,27 @@
 
 import Cocoa
 
-var str = "Hello, playground"
+
+var inputNumber = "3210423897234"
+var input: [String] = []
+var total = 0
+
+
+for Character in inputNumber.characters{
+input += [String(Character)]
+}
+
+for (var i = 0; i < 13; i++){
+    
+    if (i % 2 == 0){
+        total += Int(input[i])! *  1
+    }
+    else{
+        total += Int(input[i])! * 3
+    }
+}
+
+
+
+
+print (total)
